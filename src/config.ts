e0e0844d87ab4100af3bf9c05c6aa8c529686677
Samarth Asthan <samarthasthan@Samarths-MacBook-Air.local>
2024-03-25 23:38:07 +0530

    domain added

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://samarthasthan.github.io/", // replace this with your deployed domain
+  website: "https://samarthasthan.com/", // replace this with your deployed domain
   author: "Samarth Asthan",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "Samarth Asthan",
